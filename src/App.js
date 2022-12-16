d3ba474547eb13ad94b4de25eb33e5ebc4ca95f4
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-12-17 00:01:50 +0600

    design card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-[1440px] mx-auto">
+  <div className='grid grid-cols-3 md:gap-3 gap-[406px] justify-center overflow-y-hidden '>
+  <div className="card w-96 bg-gradient-to-r from-purple-900 to-purple-300 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div className="card w-96 bg-gradient-to-r from-purple-900 to-purple-300 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div className="card w-96 bg-gradient-to-r from-purple-900 to-purple-300 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+  </div>
+  <div className='my-6 grid sm:grid-cols-1 md:grid md:grid-cols-3 justify-center gap-3 '>
+  <div className="card w-96 bg-gradient-to-r from-white to-blue-400 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div className="card w-96 bg-gradient-to-r from-white to-green-300 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div className="card w-96 bg-gradient-to-r from-white to-orange-700 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+  </div>
+  <div>
+  <div className='my-6 grid sm:grid-cols-1 md:grid md:grid-cols-2 justify-center  '>
+  <div className="card w-96 bg-gradient-to-r from-white to-blue-400 shadow-xl mx-auto ">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+<div className="card w-96 bg-gradient-to-r from-white to-orange-700 shadow-xl mx-auto mt-4">
+  <div className="card-body">
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+  </div>
+  </div>
     </div>
   );
 }
